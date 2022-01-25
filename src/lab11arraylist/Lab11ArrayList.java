28713d396ac4d57fbebedade5c5025d28700940f
Agnieszka Gregulec <agnieszka.gregulec@gmail.com>
2022-01-25 14:40:24 +0100

    upload

---
+++ src/lab11arraylist/Lab11ArrayList.java
@@ -0,0 +1,18 @@
+package lab11arraylist;
+
+public class Lab11ArrayList {
+
+    public static void main(String[] args) {
+       Kolekcja k= new Kolekcja();
+       k.utworz();
+       k.wyswietl();
+       k.sprawdz();
+       k.usun();
+       k.drukuj();
+       k.max();
+       k.min();
+       k.sortowanie();
+       k.drukuj();
+    }
+    
+}
